f54787e9966eb58520e8c455d4d5a8204e7fea4a
Luis Felipe Gómez <gomez_pipe99@hotmail.com>
2020-11-16 17:37:32 -0500

    Actualizada la interfaz de usuario. Creada la declaracion de los metodos de los botones de la interfaz

---
+++ src/controller/MainMenuController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+public class MainMenuController {
+	
+    @FXML
+    private BorderPane mainMenu_BorderPane;
+	
+    
+    @FXML
+    void initialize() {
+		
+    }
+    
+    
+    @FXML
+    void addNewAssetClicked() {
+		
+    }
+
+    @FXML
+    void deregisterAssetClicked() {
+    	
+    }
+    
+    @FXML
+    void searchAssetClicked() {
+    	
+    }
+    
+    @FXML
+    void listAssetsClicked() {
+    	
+    }
+    
+    @FXML
+    void listDeregisteredAssetsClicked() {
+    	
+    }
+}
